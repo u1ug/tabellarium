2dcae72097819c7fa642cad53f17318081578d12
u1ug <maxim2006722@gmail.com>
2023-09-09 16:43:54 +0300

    Add logger

---
+++ pkg/logging/logging.go
@@ -0,0 +1,75 @@
+package logging
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"path"
+	"runtime"
+)
+
+type writerHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	for _, w := range hook.Writer {
+		_, err := w.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func (l *Logger) GetLoggerWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
+}
+
+func InitLogger() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.JSONFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+	}
+	err := os.MkdirAll("logs", 0777)
+	if err != nil {
+		println(err)
+	}
+
+	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+	if err != nil {
+		panic(err)
+	}
+	l.SetOutput(io.Discard)
+	l.AddHook(&writerHook{
+		Writer:    []io.Writer{allFile, os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+	l.SetLevel(logrus.TraceLevel)
+	e = logrus.NewEntry(l)
+	e.Infoln("Logger created")
+}
